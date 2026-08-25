7ed2f08765ab4ae3e6843787719b316a0f4eccbb
leipishu <kimi.peng@outlook.com>
2026-08-25 12:24:13 +0800

    package-info(current version: 1.5.2-pre)

---
+++ src/main/java/top/leipishu/tinkerssearch/client/gui/panel/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * 面板内部组件包
+ *
+ * <p>包含浮动搜索面板的内部管理类，按职责分离。</p>
+ *
+ * <h2>组件说明</h2>
+ * <ul>
+ *   <li>{@link top.leipishu.tinkerssearch.client.gui.panel.PanelDataManager} - 数据管理</li>
+ *   <li>{@link top.leipishu.tinkerssearch.client.gui.panel.PanelRenderer} - 渲染逻辑</li>
+ *   <li>{@link top.leipishu.tinkerssearch.client.gui.panel.PanelLayoutCalculator} - 布局计算</li>
+ *   <li>{@link top.leipishu.tinkerssearch.client.gui.panel.PanelAnimationManager} - 动画管理</li>
+ * </ul>
+ *
+ * @author Leipishu
+ */
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package top.leipishu.tinkerssearch.client.gui.panel;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
---
+++ src/main/java/top/leipishu/tinkerssearch/config/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 配置相关类
+ *
+ * <p>包含面板UI配置常量。</p>
+ *
+ * @author Leipishu
+ */
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package top.leipishu.tinkerssearch.config;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
